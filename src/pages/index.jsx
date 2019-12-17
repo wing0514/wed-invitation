e680e3522948bef339377cb80ef75b7bc25eab22
tsubasa_nagai <tsubasa.nagai@4digit.jp>
2019-12-18 02:09:58 +0900

    gatsby対応

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -195,6 +195,7 @@ export default class Top extends React.Component {
           </div>
         </Main>
         <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name" /></label>
           </p>
